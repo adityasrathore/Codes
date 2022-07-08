1da9eefab1c8e8e6d2cd1c3a26bd9e6c1b9c16b0
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-07-08 12:54:45 +0530

    Create 169. Majority Element.cpp

---
+++ LeetCode/169. Majority Element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int c=0,v=0;
+        for(auto i:nums){
+            if(v==0)
+                c=i;
+            if(c==i)
+                v++;
+            else 
+                v--;
+        }
+        return c;
+    }
+};
