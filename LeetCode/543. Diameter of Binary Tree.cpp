b4a72e9d4971974061e264e270069aaf284588d9
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-07-12 15:47:41 +0530

    Create 543. Diameter of Binary Tree.cpp

---
+++ LeetCode/543. Diameter of Binary Tree.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int height(TreeNode *root){
+        if(root==NULL)
+            return 0;
+        int h1=height(root->left);
+        int h2=height(root->right);
+        return 1+max(h1,h2);
+    } 
+    class HDPair{
+    public:
+    int height;
+    int diameter;
+    };
+    HDPair optDiameter(TreeNode* root){
+        HDPair p;
+        if(root==NULL){
+            p.height=p.diameter=0;
+            return p;
+        }
+        HDPair Left=optDiameter(root->left);
+        HDPair Right=optDiameter(root->right);
+        p.height=max(Left.height,Right.height)+1;
+
+        int D1= Left.height+Right.height;
+        int D2=Left.diameter;
+        int D3=Right.diameter;
+        p.diameter=max(D1,max(D2,D3));
+        return p;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        return optDiameter(root).diameter;
+    }
+};
