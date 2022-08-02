4d61a4cfe66512f4dfb470fa5936bb53d2dfdfa4
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-08-02 20:46:38 +0530

    Update 1143. Longest Common Subsequence.cpp

--- LeetCode/1143. Longest Common Subsequence.cpp
+++ LeetCode/1143. Longest Common Subsequence.cpp
@@ -19,3 +19,25 @@ public:
         return lcs(text1,text2,m,n);
     }
 };
+
+
+class Solution {
+public:
+  int t[1001][1001];
+  int lcsbottomup(string &x,string &y,int m,int n){
+    for(int i=1;i<m+1;i++)
+        for(int j=1;j<n+1;j++)
+            if(x[i-1]==y[j-1])
+                t[i][j]=1+t[i-1][j-1];
+            else    
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+
+    return t[m][n];
+}
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(t,0,sizeof(t));
+        int m=text1.length();
+        int n=text2.length();
+        return lcsbottomup(text1,text2,m,n);
+    }
+};
