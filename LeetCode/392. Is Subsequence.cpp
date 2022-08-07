e0bb8cc015c5f949883373421f02a92190ef9aa9
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-08-07 16:13:11 +0530

    Create 392. Is Subsequence.cpp

---
+++ LeetCode/392. Is Subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    bool isSubsequence(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        if(m>n)
+            return false;
+        int dp[m+1][n+1];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s[i-1]==t[j-1])
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=dp[i][j-1];
+            }
+        }
+        if(dp[m][n]==m)
+            return true;
+        else 
+            return false;
+    }
+};
