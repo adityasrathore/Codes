da839e7418e44423e42f23e7110fbe48e03e7523
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-08-22 18:40:51 +0530

    Create 1748. Sum of Unique Elements.cpp

---
+++ LeetCode/1748. Sum of Unique Elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int n=nums.size();
+        int max=*max_element(nums.begin(),nums.end());
+        int sum=0;
+        vector <int> v(max+1,0);
+        for(int i=0;i<n;i++)
+            v[nums[i]]++;
+
+        for(int i=0;i<max+1;i++)
+            if(v[i]==1)
+                sum += i*v[i];
+        return sum;
+    }
+};
