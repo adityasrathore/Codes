898e04a0c1bbaf1e9445298db17edb83d595b1d3
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-09 22:57:06 +0530

    Create 1844. Replace All Digits with Characters.cpp

---
+++ LeetCode/1844. Replace All Digits with Characters.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n=s.length();
+        for(int i=1;i<n;i+=2)
+            s[i]+=s[i-1]-'0';
+        return s;
+    }
+};
