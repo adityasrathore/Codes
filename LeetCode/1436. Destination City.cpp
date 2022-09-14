9f2be362502903acead24f2c9ae70ffa4b303a0b
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-14 21:25:52 +0530

    Update 1436. Destination City.cpp

--- LeetCode/1436. Destination City.cpp
+++ LeetCode/1436. Destination City.cpp
@@ -8,3 +8,26 @@
                 return paths[i][1];
         
         return "";
+
+        unordered_map <string,int> mp;
+        for(int i=0;i<paths.size();i++)
+            for(int j=0;j<paths[i].size();j++)
+                mp[paths[i][j]]++;
+        
+        vector <string>  v;
+        for(auto i:mp)
+            if(i.second == 1)
+                v.push_back(i.first);
+        
+        int idx=0;
+        for(int i=0;i<paths.size();i++)
+            for(int j=0;j<paths[i].size();j++)
+                if(v[0]==paths[i][j]){
+                    idx=j;
+                    break;
+                }
+        
+        if(idx%2==0)
+            return v[1];
+        else
+            return v[0];
