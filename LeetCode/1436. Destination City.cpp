49fc065c5683d7e0e93fabc8bb81d0e018d1b0b0
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-14 21:24:51 +0530

    Create 1436. Destination City.cpp

---
+++ LeetCode/1436. Destination City.cpp
@@ -0,0 +1,10 @@
+       int n=paths.size();
+        unordered_map <string,int> mp;
+        for(int i=0;i<n;i++)
+            mp[paths[i][0]]++;
+        
+        for(int i=0;i<n;i++)
+            if(mp[paths[i][1]]<1)
+                return paths[i][1];
+        
+        return "";
