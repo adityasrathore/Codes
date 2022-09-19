812337260783988f19da0ba9fc19e49136047a2a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-09-19 21:49:10 +0530

    Create 125. Valid Palindrome.cpp

---
+++ LeetCode/125. Valid Palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPalindrome(string s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j)
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else
+                return false;
+        
+        return true;
+    }
+    bool isPalindrome(string s) {
+        string new_s;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))   
+                new_s+=s[i];
+            if(s[i]>='A' && s[i]<='Z')
+                new_s+=s[i]-'A'+'a';
+        }
+        return checkPalindrome(new_s);
+    }
+};
