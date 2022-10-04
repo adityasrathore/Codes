5f12fd4180dbef04942b4606515c87b8e3ec9c32
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-04 23:13:07 +0530

    Update 287. Find the Duplicate Number.cpp

--- LeetCode/287. Find the Duplicate Number.cpp
+++ LeetCode/287. Find the Duplicate Number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+    int  findDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=1;i<n;i++)
