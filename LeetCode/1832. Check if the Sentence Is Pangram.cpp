694a53fa7b6e928d96a06d4772ed46aa1c6fe3a7
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-08 15:01:02 +0530

    Create 1832. Check if the Sentence Is Pangram.cpp

---
+++ LeetCode/1832. Check if the Sentence Is Pangram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        unordered_map <char,int> mp;
+        int n=s.size();
+        
+        for(int i=0;i<n;i++)
+            mp[s[i]]++;
+        
+        if(mp.size()==26)
+            return true;
+        
+        return false;
+    }
+};
