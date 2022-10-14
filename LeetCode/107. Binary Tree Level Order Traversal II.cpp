ad7365ad184382d7ac6b37444bc89a0a749ea319
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-14 22:18:16 +0530

    Update 107. Binary Tree Level Order Traversal II.cpp

--- LeetCode/107. Binary Tree Level Order Traversal II.cpp
+++ LeetCode/107. Binary Tree Level Order Traversal II.cpp
@@ -34,7 +34,7 @@ public:
                 v.push_back(t);
                 t.clear();
             }
-        reverse(v.begin() ,v.end());
+        reverse(v.begin(),v.end());
         return v;
     }
 };
