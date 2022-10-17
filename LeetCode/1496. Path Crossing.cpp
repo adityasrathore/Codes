b231aaaf9cda44a0f242ac281978f2cb3c088bfd
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-17 23:21:46 +0530

    Create 1496. Path Crossing.cpp

---
+++ LeetCode/1496. Path Crossing.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int x=0,y=0;
+        set <pair<int,int>> s;
+        s.insert({x,y});
+        for(char i: path){
+            if(i=='N')
+                y++;
+            else if(i=='S')
+                y--;
+            else if(i=='E')
+                x++;
+            else
+                x--;
+            if(s.find({x,y})!=s.end())
+                return true;
+            s.insert({x,y});
+        }
+        return false;
+    }
+};
