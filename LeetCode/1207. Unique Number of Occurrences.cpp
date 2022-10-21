43646a37f007fae383c3ae1d9db97f533c52b632
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-21 18:30:01 +0530

    Create 1207. Unique Number of Occurrences.cpp

---
+++ LeetCode/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map <int,int> mp;
+        for(int i=0;i<n;i++)
+            mp[arr[i]]++;
+        set <int> s;
+        
+        for(auto i:mp){
+            if(s.find(i.second)!=s.end())
+                return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
