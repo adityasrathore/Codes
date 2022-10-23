603f915d616cf343f89215ca3a5794605c24f82d
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-23 10:21:40 +0530

    Create 1704. Determine if String Halves Are Alike.cpp

---
+++ LeetCode/1704. Determine if String Halves Are Alike.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        int n=s.size();
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<n/2;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' ||s[i]=='o' || s[i]=='u')
+                count1++;
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' ||s[i]=='O' || s[i]=='U')
+                count1++;
+        }
+        for(int i=n/2;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' ||s[i]=='o' || s[i]=='u')
+                count2++;
+            if(s[i]=='A' || s[i]=='E' || s[i]=='I' ||s[i]=='O' || s[i]=='U')
+                count2++;
+        }
+        return count1==count2 ? true:false;
+    }
+};
