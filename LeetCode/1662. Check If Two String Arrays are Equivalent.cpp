202a79a6f0ec129ec2af31d0581522e8eccc0e1f
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-25 19:50:16 +0530

    Create 1662. Check If Two String Arrays are Equivalent.cpp

---
+++ LeetCode/1662. Check If Two String Arrays are Equivalent.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int m=word1.size();
+        int n=word2.size();
+        string s1="";
+        string s2="";
+        for(int i=0;i<m;i++)
+            s1+=word1[i];
+        for(int i=0;i<n;i++)
+            s2+=word2[i];
+        if(s1==s2)
+            return true;
+        return false;
+    }
+};
