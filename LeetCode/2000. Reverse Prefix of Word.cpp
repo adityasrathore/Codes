b312938fc754dd713d889310f6e45b1d06137f06
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-10-26 15:18:43 +0530

    Create 2000. Reverse Prefix of Word.cpp

---
+++ LeetCode/2000. Reverse Prefix of Word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int n=word.size();
+        int idx;
+        for(int i=0;i<n;i++){
+            if(ch==word[i]){
+                idx=i;
+                break;
+            }
+            if(i==n-1)
+                return word;
+        }
+        reverse(word.begin(),word.begin()+idx+1);
+        return word;
+    }
+};
