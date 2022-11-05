380f25aa68176b91985573c2d6ad221fb1596879
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-11-05 16:15:13 +0530

    Update 804. Unique Morse Code Words.cpp

--- LeetCode/804. Unique Morse Code Words.cpp
+++ LeetCode/804. Unique Morse Code Words.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
         vector <string> v{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        unordered_map <string,int> mp;
+        for(auto i:words){
+            string s="";
+            for(char j: i){
+                s+=v[j-'a'];
+            }
+            mp[s]++;
+        }
+        return mp.size();
+    }
+};
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector <string> v{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         unordered_set <string> mp;
         for(auto i:words){
             string s="";
