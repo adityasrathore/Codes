b9848e8a4723cb336d60e4fbbcae6bb04841ff85
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-11-07 21:55:06 +0530

    Update 494. Target Sum.cpp

--- LeetCode/494. Target Sum.cpp
+++ LeetCode/494. Target Sum.cpp
@@ -12,7 +12,7 @@ public:
             }
         }
         for(int i=1;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
+            for(int j=0;j<sum+1;j++){//IMP
                 if(nums[i-1]<=j)
                     t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j];
                 else
