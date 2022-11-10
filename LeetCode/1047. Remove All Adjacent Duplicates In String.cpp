74c64c5724a5798afcb69125ed9b011289e0c35e
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-11-10 21:38:59 +0530

    Create 1047. Remove All Adjacent Duplicates In String.cpp

---
+++ LeetCode/1047. Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+		for(int i=0;i<s.length();i++)
+			if(s[i] == ans.back())
+				ans.pop_back();
+			else
+				ans.push_back(s[i]);
+		return ans;
+    }
+};
