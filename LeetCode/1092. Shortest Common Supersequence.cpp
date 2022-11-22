12e94a0dafb4f80ff28dc95975a09fe915c58647
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-11-22 21:52:03 +0530

    Update 1092. Shortest Common Supersequence.cpp

--- LeetCode/1092. Shortest Common Supersequence.cpp
+++ LeetCode/1092. Shortest Common Supersequence.cpp
@@ -3,7 +3,7 @@ public:
      int t[1001][1001];
     string SCS(string &x,string &y,int m,int n){
     for(int i=0;i<m+1;i++){
-        for(int j=0;j<n+1;j++){
+        for(int j=0; j<n+1;j++){
             if(i==0||j==0)
                 t[i][j]=0;
         }
