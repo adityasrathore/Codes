b3a6af2064c75eb57bdea41ee174796d9cef1944
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2022-12-11 09:42:36 +0530

    Create 124. Binary Tree Maximum Path Sum.cpp

---
+++ LeetCode/124. Binary Tree Maximum Path Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int sum;
+public:
+    int maxPathSum(TreeNode* root) {
+        sum=INT_MIN;
+        help(root);
+        return sum;
+    }
+    
+    int help(TreeNode* root){
+        if(!root)   return 0;
+        int left = max(0, help(root->left));
+        int right = max(0, help(root->right));
+
+        sum = max(sum, left+right+root->val);
+      
+        return max(left, right)+root->val;
+    }
+};
