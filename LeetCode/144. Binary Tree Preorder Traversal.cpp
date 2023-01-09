af4b11164993d18987ab377592ca69baa06092a7
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-01-09 13:11:36 +0530

    Update 144. Binary Tree Preorder Traversal.cpp

--- LeetCode/144. Binary Tree Preorder Traversal.cpp
+++ LeetCode/144. Binary Tree Preorder Traversal.cpp
@@ -4,7 +4,7 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         if(root==NULL)
             return v;
-        v.push_back (root->val);
+        v.push_back(root->val);
         preorderTraversal(root->left);
         preorderTraversal(root->right);
         return v;
