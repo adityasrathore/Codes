f5a1f6a95eb68c515658d5a0056356bb313ef00c
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-02-06 10:37:05 +0530

    Update 1470. Shuffle the Array.cpp

--- LeetCode/1470. Shuffle the Array.cpp
+++ LeetCode/1470. Shuffle the Array.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector <int> ans;
+        vector <int> v;
         for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);
-            ans.push_back(nums[n+i]);
+            v.push_back(nums[i]);
+            v.push_back(nums[i+n]);
         }
-        
-        return ans;
+        return v;
+    }
+};
+// Approach-2
+// Time - O(n) Space - O(1)
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        for(int i=0;i<n;i++)
+            nums[i+n]+=nums[i]*10000;
+        for(int i=0;i<n;i++){
+            nums[2*i]=nums[i+n]/10000;
+            nums[2*i+1]=nums[i+n]%10000;
+       }
+        return nums;
+    }
+};
+// Approach-3
+// Time - O(n) Space - O(1)
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        for(int i=0;i<n;i++)
+            nums[i+n]=(nums[i+n]<<10)|nums[i];
+        for(int i=0;i<n;i++){
+            nums[2*i]=nums[i+n]&1023;
+            nums[2*i+1]=nums[i+n]>>10;
+        }
+        return nums;
     }
 };
