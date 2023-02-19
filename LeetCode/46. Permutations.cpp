eedc12c33ca323d37796904f83fb50660d671e29
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-02-19 17:49:57 +0530

    Update 46. Permutations.cpp

--- LeetCode/46. Permutations.cpp
+++ LeetCode/46. Permutations.cpp
@@ -18,3 +18,31 @@ public:
         return v;
     }
 };
+
+
+
+class Solution {
+public:
+    void solve(vector<int> &freq,vector<int> &nums,vector<int> &ans, vector<vector<int>> &v){
+        if(ans.size()==nums.size()){
+            v.push_back(ans);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]==0){
+                ans.push_back(nums[i]);
+                freq[i]=1;
+                solve(freq,nums,ans,v);
+                freq[i]=0;
+                ans.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector <int> ans;
+        vector <int> freq(nums.size(),0);
+        vector <vector<int>> v;
+        solve(freq,nums,ans,v);
+        return v;
+    }
+};
