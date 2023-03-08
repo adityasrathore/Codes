f416ea4fd25f041dcd658a113d9cf75315c8dfa5
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-08 08:00:12 +0530

    Update 875. Koko Eating Bananas.cpp

--- LeetCode/875. Koko Eating Bananas.cpp
+++ LeetCode/875. Koko Eating Bananas.cpp
@@ -25,3 +25,34 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    bool isValid(vector<int>& piles, int h,int mid){
+        long hours=0;
+        for(int i:piles){
+            hours+=i/mid;
+            if(i%mid!=0)
+                hours++;
+            if(hours>h)
+                return false;
+        }
+        return  true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int s=1,e=1e9;
+        int ans;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(isValid(piles,h,mid)==true){
+                ans=mid;
+                e=mid-1;
+            }
+            else
+                s=mid+1;
+        }
+        return ans;
+    }
+};
