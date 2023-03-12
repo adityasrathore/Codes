c5e61f49e8ba9ea8a3c407c143d1ac0fe6636006
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-12 08:11:06 +0530

    Create 23. Merge k Sorted Lists.cpp

---
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        for(auto l : lists) {
+            if(l)  
+                q.push(l);
+        }
+        if(q.empty())  
+            return NULL;
+        ListNode* result = q.top();
+        q.pop();
+        if(result->next) q.push(result->next);
+        ListNode* tail = result;            
+        while(!q.empty()) {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if(tail->next) q.push(tail->next);
+        }
+        return result;
+    }
+};
