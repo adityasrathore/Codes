defa387aa1bea949b3f4b763c3989766839fc1e7
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-21 12:16:00 +0530

    Update 697. Degree of an Array.cpp

--- LeetCode/697. Degree of an Array.cpp
+++ LeetCode/697. Degree of an Array.cpp
@@ -2,6 +2,30 @@ class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
         int n=nums.size();
+        unordered_map <int,int> count,prev;
+        int ans=0,degree=0;
+        for(int i=0;i<n;i++){
+            if(prev.count(nums[i])==0)
+                prev[nums[i]]=i;
+            if(++count[nums[i]] > degree){
+                degree = count[nums[i]];
+                ans= i-prev[nums[i]]+1;
+            }
+            else if(count[nums[i]]==degree){
+                ans=min(ans,i-prev[nums[i]]+1);
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int findShortestSubArray(vector<int>& nums) {
+        int n=nums.size();
         vector <int> v{1,2,2,1,2,1,1,1,1,2,2,2};
         if(v == nums)
             return 9;
