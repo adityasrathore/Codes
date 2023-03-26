7dffa67bd2c37d2ecd8cf40d9390b4a3fa24a5ce
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-26 10:43:51 +0530

    Update 509. Fibonacci Number.cpp

--- LeetCode/509. Fibonacci Number.cpp
+++ LeetCode/509. Fibonacci Number.cpp
@@ -12,3 +12,20 @@ public:
         return t[n];
     }
 };
+
+
+class Solution {
+public:
+    int fib(int n) {
+        int t[n+1];
+        if(n<=1)
+            return n;
+               
+        t[0]=0;
+        t[1]=1;
+        for(int i=2;i<=n;i++)
+            t[i]=t[i-1]+t[i-2];
+        
+        return t[n];
+    }
+};
