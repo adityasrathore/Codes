241a546c4f83dea6402d1a46ebfa44f28d12d173
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-28 11:16:46 +0530

    Update FrogJump.cpp

--- CodeStudio/FrogJump.cpp
+++ CodeStudio/FrogJump.cpp
@@ -16,3 +16,20 @@ int frogJump(int n, vector<int> &h)
     vector <int> t(n,-1);
     return solve(n-1,h,t);
 }
+
+
+
+
+int frogJump(int n, vector<int> &h)
+{
+    vector <int> t(n,0);
+    t[0]=0;
+    for(int i=1;i<n;i++){
+        int l=t[i-1]+abs(h[i]-h[i-1]);
+        int r=INT_MAX;
+        if(i>=2)
+            r=t[i-2]+abs(h[i]-h[i-2]);
+        t[i]=min(l,r);
+    }
+    return t[n-1];
+}
