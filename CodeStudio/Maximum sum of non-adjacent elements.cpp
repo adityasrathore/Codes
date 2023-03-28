123c5b99e995822e413d4de0dd640410cda665fc
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-28 12:09:28 +0530

    Create Maximum sum of non-adjacent elements.cpp

---
+++ CodeStudio/Maximum sum of non-adjacent elements.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int solve(int idx,vector<int> &nums,vector <int> &t){
+    if(t[idx]!=-1)
+        return t[idx];
+    if(idx==0)
+        return nums[idx];
+    if(idx<0)
+        return 0;
+    
+    int l=nums[idx]+solve(idx-2,nums,t);
+    int r=solve(idx-1,nums,t);
+    return t[idx]=max(l,r);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n=nums.size();
+    vector <int> t(n,-1);
+    return solve(n-1,nums,t);
+}
