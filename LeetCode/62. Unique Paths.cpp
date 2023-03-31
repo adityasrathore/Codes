489c44dbd1681e80005ab4f1f71d2529b22c2bde
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-03-31 16:14:58 +0530

    Update 62. Unique Paths.cpp

--- LeetCode/62. Unique Paths.cpp
+++ LeetCode/62. Unique Paths.cpp
@@ -16,3 +16,28 @@ public:
         return solve(m-1,n-1,t);
     }
 };
+
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector <vector<int>> t(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    t[i][j]=1;
+                
+                else{ 
+                    int left=0,up=0;
+                    if(i>0)
+                        up=t[i-1][j];
+                    if(j>0)
+                        left=t[i][j-1];
+                    t[i][j]=up+left;
+               }
+            }
+        }
+        return t[m-1][n-1];
+    }
+};
