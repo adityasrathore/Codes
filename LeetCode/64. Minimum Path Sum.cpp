aff78332aad6a600a030c382f27b282c533e3cf9
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-04 22:22:14 +0530

    Update 64. Minimum Path Sum.cpp

--- LeetCode/64. Minimum Path Sum.cpp
+++ LeetCode/64. Minimum Path Sum.cpp
@@ -18,9 +18,6 @@ public:
 
 
 
-
-
-
 class Solution {
 public:
     int solve(int i,int j,vector<vector<int>>& grid,vector <vector<int>> &v){
@@ -39,5 +36,33 @@ public:
         int n=grid[0].size();
         vector <vector<int>> v(m+1,vector <int> (n+1,-1));
         return solve(m-1,n-1,grid,v);
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector <vector<int>> dp(m+1,vector <int> (n+1,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0)
+                    dp[i][j]=grid[i][j];
+                
+                else{
+                    int left=1e9,up=1e9;
+                    if(i>=1)
+                        left=grid[i][j]+dp[i-1][j];
+                    if(j>=1)
+                        up=grid[i][j]+dp[i][j-1];
+                    dp[i][j]=min(left,up);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
