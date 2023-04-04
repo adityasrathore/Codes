40814d028207e65c498184e28dbdb1bf91759e87
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-04 22:02:24 +0530

    Update 120. Triangle.cpp

--- LeetCode/120. Triangle.cpp
+++ LeetCode/120. Triangle.cpp
@@ -16,3 +16,25 @@ public:
         return solve(0,0,triangle,dp);
     }
 };
+
+
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        // int dp[n][n];
+        // memset(dp,-1,sizeof(dp));
+        vector<vector<int>> dp(n,vector <int>(n,-1));
+        for(int j=n-1;j>=0;j--) // Base Case
+            dp[n-1][j]=triangle[n-1][j];
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diag=triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diag);
+            }
+        }
+        return dp[0][0];
+    }
+};
