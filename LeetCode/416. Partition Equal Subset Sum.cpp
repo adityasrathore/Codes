799e0f26a028e6060ffeef0e13da79823ad4f6ac
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-05 15:18:57 +0530

    Update 416. Partition Equal Subset Sum.cpp

--- LeetCode/416. Partition Equal Subset Sum.cpp
+++ LeetCode/416. Partition Equal Subset Sum.cpp
@@ -32,6 +32,34 @@ public:
 
 class Solution {
 public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+        int totalSum=accumulate(nums.begin(),nums.end(),0);
+        if(totalSum%2==1)
+            return false;
+        totalSum=totalSum/2;
+        vector<vector <bool>> dp(n,vector<bool>(totalSum+1,0));
+        for(int i=0;i<n;i++)
+            dp[i][0]=true;
+        for(int j=0;j<totalSum;j++)
+            dp[0][j]=false;
+        dp[0][0]=true;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=totalSum;j++){
+                bool notPick=dp[i-1][j];
+                bool pick=false;
+                if(j>=nums[i])
+                    pick=dp[i-1][j-nums[i]];
+                dp[i][j]=pick|notPick;
+            }
+        }
+        return dp[n-1][totalSum];
+    }
+};
+
+
+class Solution {
+public:
     int solve(int idx,int target,vector<int>& nums,vector<vector<int>> &dp){
         if(target==0)
             return true;
@@ -56,3 +84,7 @@ public:
         return solve(n-1,target/2,nums,dp);
     }
 };
+
+
+
+
