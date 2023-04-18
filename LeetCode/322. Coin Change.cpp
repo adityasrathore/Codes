3af346f1a70a5836052240c2f727b3c7dca6cdc6
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-18 13:01:25 +0530

    Update 322. Coin Change.cpp

--- LeetCode/322. Coin Change.cpp
+++ LeetCode/322. Coin Change.cpp
@@ -21,3 +21,31 @@ public:
         return ans==1e9?-1:ans;
     }
 };
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int j=0;j<=amount;j++){
+            if(j%coins[0]==0)
+                dp[0][j] = (j/coins[0]);
+            else
+                dp[0][j] = 1e9;
+        }
+
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                int notPick=dp[i-1][j];
+                int pick=INT_MAX;
+                if(j>=coins[i])
+                    pick=1+dp[i][j-coins[i]];
+                dp[i][j]=min(pick,notPick);
+            }
+        }
+        int ans=dp[n-1][amount];
+        return ans==1e9?-1:ans;
+    }
+};
