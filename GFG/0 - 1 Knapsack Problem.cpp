65c1ac5213345a93cfa45874b17bfc1c8cf9cbb5
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-18 11:27:21 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- GFG/0 - 1 Knapsack Problem.cpp
+++ GFG/0 - 1 Knapsack Problem.cpp
@@ -17,3 +17,23 @@
         vector<vector<int>> dp(n,vector<int>(W+1,-1));
         return solve(n-1,wt,val,W,dp);
     }
+
+
+    int knapSack(int W, int wt[], int val[], int n){ 
+        // vector<vector<int>> dp(n,vector<int>(W+1,-1));
+        // return solve(n-1,wt,val,W,dp);
+        vector<vector<int>> dp(n,vector<int>(W+1,0));
+        for(int j=wt[0];j<=W;j++){
+            dp[0][j]=val[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=W;j++){
+                int notPick=dp[i-1][j];
+                int pick=INT_MIN;
+                if(j>=wt[i])
+                    pick=val[i]+dp[i-1][j-wt[i]];
+                dp[i][j]=max(pick,notPick);
+            }
+        }
+        return dp[n-1][W];
+    }
