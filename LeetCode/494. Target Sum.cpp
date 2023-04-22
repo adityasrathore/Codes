9c3337614a6a8feefc5b20b9d5fb131388b63b54
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-04-22 09:40:57 +0530

    Update 494. Target Sum.cpp

--- LeetCode/494. Target Sum.cpp
+++ LeetCode/494. Target Sum.cpp
@@ -30,3 +30,33 @@ public:
         return countSubset(nums,s1);
     }
 };
+
+
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        target=abs(target);
+        int total=accumulate(nums.begin(),nums.end(),0);
+        if((total-target)%2==1 || (total-target)<0)
+            return 0;
+        target=(total-target)/2;
+        vector <vector<int>> dp(n,vector<int>(target+1,0));
+        for(int i=0;i<n;i++)
+            dp[i][0]=1;
+        if(nums[0]<=target)
+            dp[0][nums[0]]=1;
+        if(nums[0]==0)
+            dp[0][0]=2;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int notPick=dp[i-1][j];
+                int pick=0;
+                if(j>=nums[i])
+                    pick=dp[i-1][j-nums[i]];
+                dp[i][j]=pick + notPick;
+            }
+        }
+        return dp[n-1][target];
+    }
+};
