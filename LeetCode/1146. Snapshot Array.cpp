d4d656d586adb4b3604c2886a6d58d44ce73ce45
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-11 22:10:17 +0530

    Create 1146. Snapshot Array.cpp

---
+++ LeetCode/1146. Snapshot Array.cpp
@@ -0,0 +1,24 @@
+class SnapshotArray {
+public:
+    map<int, map<int, int>> snaps;
+    int snapId = 0;
+    SnapshotArray(int length) {
+        for (int i = 0; i < length; i++) {
+            map<int, int> mp; mp[0] = 0;
+            snaps[i] = mp;
+        }
+    }
+    
+    void set(int index, int val) {
+        snaps[index][snapId] = val;
+    }
+    
+    int snap() {
+        return snapId++;
+    }
+    
+    int get(int index, int snap_id) {
+        auto it = snaps[index].upper_bound(snap_id); it--;
+        return it->second;
+    }
+};
