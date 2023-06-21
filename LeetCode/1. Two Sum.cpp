daabb27d1bff8d46927fc54a7f35731d68716fcb
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-21 22:00:34 +0530

    Update 1. Two Sum.cpp

--- LeetCode/1. Two Sum.cpp
+++ LeetCode/1. Two Sum.cpp
@@ -4,8 +4,7 @@ public:
         vector <int> v;
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
-                if (nums[i]+nums[j] ==target)
-                { 
+                if (nums[i]+nums[j] ==target){ 
                     v.push_back(i);
                     v.push_back(j);
                 break;
