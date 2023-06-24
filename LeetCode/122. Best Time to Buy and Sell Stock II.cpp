0fd712dce0368b6f01e444503928c0abfdf76dee
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-24 13:19:59 +0530

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- LeetCode/122. Best Time to Buy and Sell Stock II.cpp
+++ LeetCode/122. Best Time to Buy and Sell Stock II.cpp
@@ -19,3 +19,29 @@ public:
         return solve(0,prices,0);
     }
 };
+
+
+class Solution {
+public:
+    // opt = 0 (Buy) 
+    // opt = 1 (Sell)
+    int solve(int idx,vector<int>& prices,int opt,vector<vector<int>> &dp){
+        if(idx == prices.size())
+            return 0;
+        if(dp[idx][opt] != -1)
+            return dp[idx][opt];
+        int profit = 0;
+        if(opt == 0){
+            profit = max(-prices[idx]+solve(idx+1,prices,1,dp),solve(idx+1,prices,0,dp));
+        }
+        else{
+            profit = max(prices[idx]+solve(idx+1,prices,0,dp),solve(idx+1,prices,1,dp));
+        }
+        return dp[idx][opt] = profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return solve(0,prices,0,dp);
+    }
+};
