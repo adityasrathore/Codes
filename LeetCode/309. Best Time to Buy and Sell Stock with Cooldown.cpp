c717ebc139983c365902ad52c680f9b363bc13b1
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-25 13:01:32 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp

--- LeetCode/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ LeetCode/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -20,3 +20,18 @@ public:
         return solve(0,0,prices,dp);
     }
 };
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        for(int i=n-1;i>=0;i--){
+            dp[i][0] = max(-prices[i]+dp[i+1][1],dp[i+1][0]);
+            dp[i][1] = max(prices[i]+dp[i+2][0],dp[i+1][1]);
+        }
+        return dp[0][0];
+    }
+};
