3611a7ff49f6cf9da61c1fcebd26632ac9f0f323
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-28 12:38:12 +0530

    Update 84. Largest Rectangle in Histogram.cpp

--- LeetCode/84. Largest Rectangle in Histogram.cpp
+++ LeetCode/84. Largest Rectangle in Histogram.cpp
@@ -40,3 +40,56 @@ public:
     
     }
 };
+
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        vector<int> ngr(n);
+        vector<int> ngl(n);
+        stack<pair<int,int>> s;
+        // Next Smaller Right
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && s.top().first >= heights[i])
+                s.pop();
+            if(s.empty())
+                ngr[i] = n;
+            else
+                ngr[i] = s.top().second;
+            
+            s.push({heights[i],i});
+        }
+        
+        //Empty the stack
+        while(s.size()>0)
+            s.pop();
+        
+        // Next Smaller Left
+        for(int i=0;i<n;i++){
+            while(!s.empty() && s.top().first >= heights[i])
+                s.pop();
+            if(s.empty())
+                ngl[i] = -1;
+            else
+                ngl[i] = s.top().second;
+            
+            s.push({heights[i],i});
+        }
+        // Calculating the Width 
+        vector<int> width(n);
+        for(int i=0;i<n;i++)
+            width[i] = ngr[i]-ngl[i]-1;
+        
+        // Finding the Maximum Area that is possible
+        vector<int> area(n);
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            area[i] = heights[i]*width[i];
+            maxArea = max(maxArea,area[i]);
+        }
+        
+        return maxArea;
+    }
+};
