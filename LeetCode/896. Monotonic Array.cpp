cec06a9f6f98f0423ee09a69d720d568840bedea
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-28 17:47:04 +0530

    Create 896. Monotonic Array.cpp

---
+++ LeetCode/896. Monotonic Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        if(nums[0] <= nums[n-1]){
+            for(int i=0;i<n-1;i++){
+                if(nums[i] > nums[i+1])
+                    return false;
+            }
+        }
+        else{
+            for(int i=0;i<n-1;i++){
+                if(nums[i] < nums[i+1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
