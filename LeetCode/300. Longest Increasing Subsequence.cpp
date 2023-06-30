9273d54e564a88b0ce969e45dc277943e4051f61
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-06-30 18:37:44 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- LeetCode/300. Longest Increasing Subsequence.cpp
+++ LeetCode/300. Longest Increasing Subsequence.cpp
@@ -1,20 +1,56 @@
+Recursion
+
 class Solution {
 public:
-    int t[2501][2501];
-    int solve(int idx,int prev_idx,vector<int> &nums){
-        if(idx==nums.size())
+    int solve(int idx,int prev,vector<int>& nums){
+        if(idx == nums.size())
             return 0;
-        if(t[idx][prev_idx+1]!=-1)
-            return t[idx][prev_idx+1];
-        int len=0 + solve(idx+1,prev_idx,nums);
-        
-        if(prev_idx == -1 || nums[idx]>nums[prev_idx])
-             len=max(len,1+solve(idx+1,idx,nums));
-        
-        return t[idx][prev_idx+1]=len;
+        int len = 0;
+        len = solve(idx+1,prev,nums);
+        if(prev == -1 || nums[idx] > nums[prev])
+            len = max(len,1 + solve(idx+1,idx,nums));
+        return len;
     }
-    int lengthOfLIS(vector<int> &nums) {
-        memset(t,-1,sizeof(t));
+    int lengthOfLIS(vector<int>& nums) {
+
         return solve(0,-1,nums);
+    }
+};
+Recursion + Storing = Memoization
+
+class Solution {
+public:
+    int solve(int idx,int prev,vector<int>& nums,vector<vector<int>> &dp){
+        if(idx == nums.size())
+            return 0;
+        if(dp[idx][prev+1] != -1)
+            return dp[idx][prev+1] ;
+        int len = 0;
+        len = solve(idx+1,prev,nums,dp);
+        if(prev == -1 || nums[idx] > nums[prev])
+            len = max(len,1 + solve(idx+1,idx,nums,dp));
+        return dp[idx][prev+1] = len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return solve(0,-1,nums,dp);
+    }
+};
+Tabulation BotoomUp
+
+ class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i-1;j>=-1;j--){
+                dp[i][j+1] = dp[i+1][j+1];
+                if(j == -1 || nums[i] > nums[j])
+                    dp[i][j+1] = max(dp[i+1][j+1],1 + dp[i+1][i+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
