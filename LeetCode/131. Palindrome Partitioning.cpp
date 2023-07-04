0467ec2be5536bd335253b62211bc2c45a9307d6
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-07-04 11:53:36 +0530

    Update 131. Palindrome Partitioning.cpp

--- LeetCode/131. Palindrome Partitioning.cpp
+++ LeetCode/131. Palindrome Partitioning.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string> > ret;
-        if(s.empty()) return ret;
-        
-        vector<string> path;
-        dfs(0, s, path, ret);
-        
-        return ret;
-    }
-    
-    void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret) {
-        if(index == s.size()) {
-            ret.push_back(path);
-            return;
-        }
-        for(int i = index; i < s.size(); ++i) {
-            if(isPalindrome(s, index, i)) {
-                path.push_back(s.substr(index, i - index + 1));
-                dfs(i+1, s, path, ret);
-                path.pop_back();
-            }
-        }
-    }
-    
-    bool isPalindrome(const string& s, int start, int end) {
-        while(start <= end) {
-            if(s[start++] != s[end--])
+    bool checkPalindrome(string &str,int s,int e){
+        int n = str.size();
+        while(s<=e){
+            if(str[s++] != str[e--])
                 return false;
         }
         return true;
+    }
+    void solve(int idx,string &s,vector<string> &temp,vector<vector<string>> &v){
+        if(idx == s.size()){
+            v.push_back(temp);
+            return;
+        }
+        string str = "";
+        for(int i=idx;i<s.size();i++){
+            str += s[i];
+            if(checkPalindrome(s,idx,i) == true){
+                temp.push_back(str);
+                solve(i+1,s,temp,v);
+                temp.pop_back(); // Backtrack
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string> temp; 
+        vector<vector<string>> v;
+        solve(0,s,temp,v);
+        return v;
     }
 };
