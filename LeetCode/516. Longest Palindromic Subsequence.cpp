aca7cb7b1abb61806e8ef9a486174a2d4a868cc6
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-07-08 20:48:36 +0530

    Update 516. Longest Palindromic Subsequence.cpp

--- LeetCode/516. Longest Palindromic Subsequence.cpp
+++ LeetCode/516. Longest Palindromic Subsequence.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        string t = s;
+        reverse(s.begin(),s.end());
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == t[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+        return dp[n][n];
+    }
+};
+
+-----------------------------------------
+
+
+class Solution {
+public:
     int t[1001][1001];
     int LPS(string &x,int m){
     string y=x;
