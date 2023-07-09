6910a6af766dac460880206192578949a7ed8617
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-07-09 12:30:39 +0530

    Create 832. Flipping an Image.cpp

---
+++ LeetCode/832. Flipping an Image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(image[i][j],image[i][n-j-1]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(image[i][j] == 1)
+                    image[i][j] = 0;
+                else
+                    image[i][j] = 1;
+            }
+        }
+        return image;
+    }
+};
