59eba5f9e7ceba59da57e3007ee6d6d8cc161e30
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-07-25 20:21:37 +0530

    Update 100. Same Tree.cpp

--- LeetCode/100. Same Tree.cpp
+++ LeetCode/100. Same Tree.cpp
@@ -19,3 +19,13 @@ public:
                 isSameTree(p->right, q->right));
     }
 };
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL)
+            return p == q;
+        return p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
