028893bae84818f06833052da4f38f6e2bccaae3
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-07-25 13:16:11 +0530

    Update 852. Peak Index in a Mountain Array.cpp

--- LeetCode/852. Peak Index in a Mountain Array.cpp
+++ LeetCode/852. Peak Index in a Mountain Array.cpp
@@ -16,3 +16,24 @@
             i=mid+1;
     }
     return a;
+
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int s = 1;
+        int e = n-2;
+        
+        while(s<=e){
+            int mid = s + (e-s)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
+                return mid;
+            if(arr[mid]>arr[mid-1])
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+        return -1;
+    }
+};
