af1ad16d0b8e8217302d9d035bab9c199afd0cd1
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-06 23:49:31 +0530

    Create 920. Number of Music Playlists.cpp

---
+++ LeetCode/920. Number of Music Playlists.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    int numMusicPlaylists(int n, int goal, int k) {
+         vector<vector<long>> dp(goal+1,vector<long>(n+1,0));
+        
+         dp[0][0] = 1;      
+         for(int i=1;i<=goal;i++){
+             for(int j=1;j<=n;j++){
+                 dp[i][j] = dp[i-1][j-1]*(n-j+1);
+                 
+                 dp[i][j] = (dp[i][j]+dp[i-1][j]*max(j-k,0))%MOD; 
+             }
+         }
+        return dp[goal][n]%MOD;
+    }
+};
