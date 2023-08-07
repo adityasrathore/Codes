521eea0c5ebb7e319c25a6beef854a45adf3840c
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-07 08:12:04 +0530

    Update 74. Search a 2D Matrix.cpp

--- LeetCode/74. Search a 2D Matrix.cpp
+++ LeetCode/74. Search a 2D Matrix.cpp
@@ -16,3 +16,26 @@ public:
         return false;
     }
 };
+
+
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // cout<<m<<" "<<n<<endl;
+        int i = 0;
+        int j = n - 1;
+        while(i < m && j >= 0){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                j--;
+            else
+                i++;
+        }
+        return false;
+    }
+};
