d03b1823c4e6b94ccb77586e30d3434c6f057c2a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-11 16:30:09 +0530

    Update 518. Coin Change 2.cpp

--- LeetCode/518. Coin Change 2.cpp
+++ LeetCode/518. Coin Change 2.cpp
@@ -15,10 +15,35 @@ public:
             for(int j=1;j<sum+1;++j){
                 if(coins[i-1]<=j)
                     t[i][j]=(t[i][j-coins[i-1]]+t[i-1][j]);
-                else
+                elsea
                     t[i][j]=t[i-1][j];
             }
         }
+        return t[n][sum];
+    }
+    
+};
+class Solution {
+public:
+    int t[301][15001];
+    int change(int sum, vector<int>& coins) {
+        int n=coins.size();
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)
+                    t[i][j]=0;
+                if(j==0)
+                    t[i][j]=1;
+            }
+        }
+        for(int i=1;i<n+1;++i)
+            for(int j=1;j<sum+1;++j)
+                if(coins[i-1]<=j)
+                    t[i][j]=(t[i][j-coins[i-1]]+t[i-1][j]);
+                else
+                    t[i][j]=t[i-1][j];
+            
+        
         return t[n][sum];
     }
     
