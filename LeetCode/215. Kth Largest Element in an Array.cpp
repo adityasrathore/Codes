79acbdf99daebc44279b14fa00803900d92de07a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-14 21:06:53 +0530

    Update 215. Kth Largest Element in an Array.cpp

--- LeetCode/215. Kth Largest Element in an Array.cpp
+++ LeetCode/215. Kth Largest Element in an Array.cpp
@@ -5,3 +5,21 @@ public:
         return nums[k-1];
     }
 };
+
+
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue <int> mh;
+        for(auto i:nums){
+            mh.push(-i);
+            if(mh.size() > k)
+                mh.pop();
+        }
+        return mh.top()*-1;
+    }
+};
+
+
