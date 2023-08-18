08574962ca7e87100005863325b742191413fdbe
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-18 16:18:50 +0530

    Update BFS of graph.cpp

--- GFG/BFS of graph.cpp
+++ GFG/BFS of graph.cpp
@@ -22,3 +22,26 @@ class Solution {
         return v;
     }
 };
+
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        int vis[V] = {0};
+        vis[0] = 1;
+        queue <int> q;
+        q.push(0);
+        vector <int> v;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            v.push_back(node);
+            for(auto i:adj[node]){
+                if(vis[i] != 1)
+                    q.push(i);
+                    vis[i] = 1;
+            }
+        }
+        return v;
+    }
+};
