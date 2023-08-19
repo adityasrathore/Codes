7bdc90432343991b8dcd1d063138d4c32a775b60
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-19 22:40:21 +0530

    Update 994. Rotting Oranges.cpp

--- LeetCode/994. Rotting Oranges.cpp
+++ LeetCode/994. Rotting Oranges.cpp
@@ -45,3 +45,53 @@ public:
         return time;
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m));
+        queue <pair<pair<int,int>,int>> q;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    vis[i][j] = 2;
+                }
+                else
+                    vis[i][j] = 0;
+            }
+        }
+        int a[4] = {-1,0,1,0};
+        int b[4] = {0,1,0,-1};
+        int time = 0;
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            time = max(time,t);
+            for(int i=0;i<4;i++){
+                int nrow = row + a[i];
+                int ncol = col + b[i];
+                if(nrow>=0 && nrow < n && ncol >=0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol] = 2; 
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(vis[i][j] != 2 && grid[i][j] == 1)
+                    return -1;
+        
+        return time;
+    }
+};
