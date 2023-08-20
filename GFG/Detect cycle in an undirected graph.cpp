b3de155faf34a88d1f06db9dbc038599a94573e8
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-20 17:06:04 +0530

    Update Detect cycle in an undirected graph.cpp

--- GFG/Detect cycle in an undirected graph.cpp
+++ GFG/Detect cycle in an undirected graph.cpp
@@ -1,3 +1,4 @@
+-------BFS--------
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
@@ -25,6 +26,36 @@ class Solution {
         for(int i=0;i<V;i++){
             if(vis[i] != 1)
                 if(detectCycle(i,vis,V,adj) == true)
+                    return true;
+        }
+        return false;
+    }
+};
+
+
+
+-----------DFS-------
+
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool detectCycleDFS(int node,int parent,vector<int> &vis, vector<int> adj[]){
+        vis[node] = 1;
+        for(auto i:adj[node]){
+            if(vis[i] == 0){
+                if(detectCycleDFS(i,node,vis,adj))
+                    return true;
+            }
+            else if(parent != i)
+                return true;
+        }
+        return false;
+    }
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> vis(V);
+        for(int i=0;i<V;i++){
+            if(vis[i] == 0)
+                if(detectCycleDFS(i,-1,vis,adj) == true)
                     return true;
         }
         return false;
