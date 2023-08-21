23de00e07c9a7f4550b4fb569a47ef231c219b0a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-08-21 15:30:07 +0530

    Update 198. House Robber.cpp

--- LeetCode/198. House Robber.cpp
+++ LeetCode/198. House Robber.cpp
@@ -13,3 +13,25 @@ public:
         return f[n-1];
     }
 };
+
+
+
+class Solution {
+public:
+    int solve(int idx,vector<int>& nums,vector<int> &dp){
+        if(idx == 0)
+            return nums[0];
+        if(idx < 0)
+            return 0;
+        if(dp[idx] != -1)
+            return dp[idx];
+        int p = nums[idx] + solve(idx-2,nums,dp);
+        int notp = solve(idx-1,nums,dp);
+        return dp[idx] = max(p,notp);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return solve(n-1,nums,dp);
+    }
+};
