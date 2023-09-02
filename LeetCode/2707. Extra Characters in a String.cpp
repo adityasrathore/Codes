1a3237b934ef44c12d8675f2209801f247a8bb3d
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-02 20:47:08 +0530

    Create 2707. Extra Characters in a String.cpp

---
+++ LeetCode/2707. Extra Characters in a String.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(string& s, unordered_map<string, int>&mp, vector<int>&dp, int index)
+    {
+        if (index >= s.size()) return 0;
+        if (dp[index] != -1) return dp[index]; 
+        
+        string currStr = "";
+        int minExtra = s.size();
+        for (int cutIdx = index; cutIdx < s.size(); cutIdx++)
+        {
+            currStr.push_back(s[cutIdx]);
+            int currExtra = (mp.count(currStr))? 0 : currStr.size();
+            int nextExtra = solve(s, mp, dp, cutIdx + 1);
+            int totalExtra = currExtra + nextExtra;
+            
+            minExtra = min(minExtra, totalExtra);
+        }
+        return dp[index] = minExtra;
+    }
+    int minExtraChar(string s, vector<string>& dictionary) 
+    {
+        vector<int>dp(s.size(), -1);
+        unordered_map<string, int>mp;
+        for (string& word : dictionary) mp[word]++;
+        
+        int ans = solve(s, mp, dp, 0);
+        return ans;
+        
+    }
+};
