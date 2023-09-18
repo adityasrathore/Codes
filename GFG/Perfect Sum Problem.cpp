ee3d12fb8582ad4766d6f39a6078e27e0deb2913
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-18 18:57:02 +0530

    Update Perfect Sum Problem.cpp

--- GFG/Perfect Sum Problem.cpp
+++ GFG/Perfect Sum Problem.cpp
@@ -1,4 +1,32 @@
 class Solution{
+    int MOD = 1e9 + 7;
+	public:
+	int solve(int idx,int arr[], int n, int sum,vector<vector<int>> &dp){
+	    if(idx == 0){
+	        if(arr[0] == 0 && sum == 0)
+	            return 2;
+	        else if(arr[0] == sum || sum == 0)
+	            return 1;
+	       else
+	        return 0;
+	    }
+
+	    if(dp[idx][sum] != -1)
+	        return dp[idx][sum];
+	    int notTake = solve(idx-1,arr,n,sum,dp);
+	    int take = 0;
+	    if(arr[idx] <= sum)
+	        take = solve(idx-1,arr,n,sum-arr[idx],dp);
+	    return dp[idx][sum] = (take + notTake)%MOD;
+	}
+	int perfectSum(int arr[], int n, int sum){
+	    vector<vector<int>> dp(n,vector<int>(sum+1,-1));
+        return solve(n-1,arr,n,sum,dp);
+	}
+	  
+};
+
+class Solution{
 
 	public:
 	int perfectSum(int arr[], int n, int sum){
