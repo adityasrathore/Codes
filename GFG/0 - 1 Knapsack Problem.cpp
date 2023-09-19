79ea17249907747b744448c607c4eb367aeda239
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-19 14:59:00 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- GFG/0 - 1 Knapsack Problem.cpp
+++ GFG/0 - 1 Knapsack Problem.cpp
@@ -1,24 +1,29 @@
-    int solve(int idx,int wt[], int val[],int w,vector<vector<int>> &dp){
-        if(idx==0){
-            if(wt[0]<=w){
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int solve(int idx,int W, int wt[], int val[], int n,vector<vector<int>> &dp){
+        if(idx == 0){
+            if(wt[0] <= W)
                 return val[0];
-            }
             return 0;
         }
-        if(dp[idx][w]!=-1)
-            return dp[idx][w];
-        int notPick=solve(idx-1,wt,val,w,dp);
-        int pick=INT_MIN;
-        if(wt[idx]<=w)
-            pick=val[idx]+solve(idx-1,wt,val,w-wt[idx],dp);
-        return dp[idx][w]=max(pick,notPick);
+        if(dp[idx][W] != -1)
+            return dp[idx][W];
+        int notTake = solve(idx-1,W,wt,val,n,dp);
+        int take = INT_MIN;
+        if(wt[idx] <= W){
+            take = val[idx] + solve(idx-1,W-wt[idx],wt,val,n,dp);
+        }
+        return dp[idx][W] = max(take,notTake);
     }
     int knapSack(int W, int wt[], int val[], int n){ 
         vector<vector<int>> dp(n,vector<int>(W+1,-1));
-        return solve(n-1,wt,val,W,dp);
+        return solve(n-1,W,wt,val,n,dp);
     }
+};
 
-
+-----------------------------------------------------
     int knapSack(int W, int wt[], int val[], int n){ 
         // vector<vector<int>> dp(n,vector<int>(W+1,-1));
         // return solve(n-1,wt,val,W,dp);
