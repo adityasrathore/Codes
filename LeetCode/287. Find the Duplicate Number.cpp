a4ad7139c5afb07fbf6b6bd179bcfc287948452b
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-19 21:04:05 +0530

    Update 287. Find the Duplicate Number.cpp

--- LeetCode/287. Find the Duplicate Number.cpp
+++ LeetCode/287. Find the Duplicate Number.cpp
@@ -9,3 +9,18 @@ public:
         return 0;
     }
 };
+---------------------
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map <int,int> mp;
+        for(auto i:nums)
+            mp[i]++;
+        
+        for(auto i:mp)
+            if(i.second > 1)
+                return i.first;
+        return -1;
+    }
+};
