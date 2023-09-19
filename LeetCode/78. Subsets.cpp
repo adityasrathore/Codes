125363d22d1eae1683ea6694bdcc646798f01742
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-19 14:24:17 +0530

    Update 78. Subsets.cpp

--- LeetCode/78. Subsets.cpp
+++ LeetCode/78. Subsets.cpp
@@ -17,3 +17,23 @@ public:
         return v;
     }
 };
+
+---------------------------------------------------------------
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int p = (1 << n);
+        for(int i=0;i<p;i++){
+            vector<int> v;
+            for(int j=0;j<n;j++){
+                if(i & (1<<j))
+                    v.push_back(nums[j]);
+            }
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
