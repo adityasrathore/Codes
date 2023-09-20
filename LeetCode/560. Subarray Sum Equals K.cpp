879fd6b3b0d321ccd6869ae270d2e7fc2b88af3d
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-21 00:14:52 +0530

    Update 560. Subarray Sum Equals K.cpp

--- LeetCode/560. Subarray Sum Equals K.cpp
+++ LeetCode/560. Subarray Sum Equals K.cpp
@@ -21,3 +21,22 @@ public:
         return count;
     }
 }
+
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        mp[0]++;
+        int prefSum = 0;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            prefSum += nums[i];
+            if(mp[prefSum - k] > 0)
+                count += mp[prefSum - k];
+            mp[prefSum]++;
+        }
+        return count;
+    }
+};
