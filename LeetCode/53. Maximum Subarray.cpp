6bbfc9450480f52f524255c2599f22824077b786
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-20 23:21:43 +0530

    Update 53. Maximum Subarray.cpp

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int max = *max_element(nums.begin(),nums.end());
+        if(max < 0)
+            return max;
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(sum < 0)
+                sum = 0;
+            if(sum > maxSum)
+                maxSum = sum;
+        }
+        return maxSum;
+    }
+};
+---------------------------------------------
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
         int n=nums.size(),sum=0,maxprofit=INT_MIN;
         
         for(int i=0;i<n;i++){
