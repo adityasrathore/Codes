76ec84fe34a83a4a9119fdf2405a9210dfeac9ce
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-21 18:35:53 +0530

    Update 1. Two Sum.cpp

--- LeetCode/1. Two Sum.cpp
+++ LeetCode/1. Two Sum.cpp
@@ -1,3 +1,4 @@
+Sol-1-Using O(n2)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -14,5 +15,20 @@ public:
         }
         
         return v;
+    }
+};
+
+Sol-2-Using Maps T-O(n) S-O(n) 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map <int,int> mp;
+        for(int i=0;i<n;i++){
+            if(mp.find(target - nums[i]) != mp.end())
+                return {i,mp[target - nums[i]]};
+            mp[nums[i]] = i;
+        }
+        return {};
     }
 };
