c5517347a943c17b879a8547556834e7e0ea808a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-22 22:55:24 +0530

    Update 392. Is Subsequence.cpp

--- LeetCode/392. Is Subsequence.cpp
+++ LeetCode/392. Is Subsequence.cpp
@@ -1,3 +1,26 @@
+Two-Pointers
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int j = 0;
+        for(int i=0;i<n;i++){
+            if(t[i] == s[j])
+                j++;
+        }
+        if(j == m)
+            return true;
+        return false;
+    }
+};
+
+
+---------------------------------
+
+DP
+
 class Solution {
 public:
     
