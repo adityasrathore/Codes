cb88f6a687fc0fcfdfc48fb71b3cb65fbe75ce8a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-09-25 14:11:04 +0530

    Update 389. Find the Difference.cpp

--- LeetCode/389. Find the Difference.cpp
+++ LeetCode/389. Find the Difference.cpp
@@ -11,3 +11,19 @@ public:
         return sumt-sums; //ASCII Value of that Extra Character
     }
 };
+
+
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n=s.length();
+        int sums = 0;
+        int sumt = t[n];
+        for(int i=0;i<n;i++){
+            sumt += t[i];
+            sums += s[i];
+        }
+        return char(sumt-sums);
+    }
+};
