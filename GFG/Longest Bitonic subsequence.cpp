47bd360c2925fac69ad5a40600e3c2553e0fbc1a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-10-18 12:20:22 +0530

    Update Longest Bitonic subsequence.cpp

--- GFG/Longest Bitonic subsequence.cpp
+++ GFG/Longest Bitonic subsequence.cpp
@@ -29,3 +29,32 @@ class Solution{
 	    return *max_element(ans.begin(),ans.end());
 	}
 };
+
+class Solution{
+	public:
+	int LongestBitonicSequence(vector<int>nums){
+	    int n = nums.size();
+	    vector<int> dp1(n,1);
+	    vector<int> dp2(n,1);
+	    int maxLen1 = 1;
+	    int maxLen2 = 1;
+	    for(int i=1;i<n;i++){
+	        for(int j=0;j<i;j++){
+	            if(nums[i] > nums[j] && dp1[i] < 1 + dp1[j])
+	                dp1[i] = 1 + dp1[j];
+	        }
+	    }
+	    for(int i=n-2;i>=0;i--){
+	        for(int j=n-1;j>i;j--){
+	            if(nums[i] > nums[j] && dp2[i] < 1 + dp2[j])
+	                dp2[i] = 1 + dp2[j];
+	        }
+	    }
+	    int sum = 0;
+	    for(int i=0;i<n;i++){
+	        dp1[i] += dp2[i]; 
+	        sum = max(sum,dp1[i]);
+	    }
+	    return sum - 1;
+	}
+};
