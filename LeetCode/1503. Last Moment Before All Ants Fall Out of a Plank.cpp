a8208b2b991422bf1e3b9cbf1971974e54c9f8fb
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-11-04 21:49:47 +0530

    Create 1503. Last Moment Before All Ants Fall Out of a Plank.cpp

---
+++ LeetCode/1503. Last Moment Before All Ants Fall Out of a Plank.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int res = 0;
+        if(left.size()) {
+            int t = *max_element(left.begin(), left.end());
+            res = t;
+        }
+        if(right.size()) {
+            int t = *min_element(right.begin(), right.end());
+            res = max(res, n-t);
+        }
+        
+        return res;
+    }
+};
