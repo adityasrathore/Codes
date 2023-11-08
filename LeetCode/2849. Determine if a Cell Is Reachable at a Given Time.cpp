a206d428458bb226984a488b7d300188291d66f5
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-11-08 22:53:12 +0530

    Create 2849. Determine if a Cell Is Reachable at a Given Time.cpp

---
+++ LeetCode/2849. Determine if a Cell Is Reachable at a Given Time.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xdiff = abs(sx - fx);
+        int ydiff = abs(sy - fy);
+        if(xdiff == 0 && ydiff == 0 && t == 1)
+            return false;
+        return max(xdiff, ydiff) <= t;
+    }   
+};
