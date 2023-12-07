0f1d8aaf8fef48129ade9284deadae698e077379
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-12-07 17:02:51 +0530

    Update 1903. Largest Odd Number in String.cpp

--- LeetCode/1903. Largest Odd Number in String.cpp
+++ LeetCode/1903. Largest Odd Number in String.cpp
@@ -11,3 +11,17 @@ public:
         return "";
     }
 };
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i=n-1;i>=0;i--){ 
+            if(num[i]%2 == 0) 
+                num.pop_back();
+            else 
+                return num;
+        }
+	    return "";
+    }
+};
