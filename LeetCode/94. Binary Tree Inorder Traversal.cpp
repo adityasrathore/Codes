81e80ceff22c3a7d4616327dd133fb2ec0f345fe
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-12-09 09:52:18 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- LeetCode/94. Binary Tree Inorder Traversal.cpp
+++ LeetCode/94. Binary Tree Inorder Traversal.cpp
@@ -10,3 +10,17 @@ public:
         return v;
     }
 };
+
+
+class Solution {
+public:
+    vector <int> v;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==NULL)
+            return v;
+        inorderTraversal(root->left);
+        v.push_back(root->val);
+        inorderTraversal(root->right);
+        return v;
+    }
+};
