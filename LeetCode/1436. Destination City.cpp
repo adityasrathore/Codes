cfd6ca2d5906089493b4c6a346587619266ae9c8
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-12-15 19:51:26 +0530

    Update 1436. Destination City.cpp

--- LeetCode/1436. Destination City.cpp
+++ LeetCode/1436. Destination City.cpp
@@ -1,4 +1,29 @@
-       int n=paths.size();
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+//         unordered_map <string,int> mp;
+//         for(int i=0;i<paths.size();i++)
+//             for(int j=0;j<paths[i].size();j++)
+//                 mp[paths[i][j]]++;
+        
+//         vector <string>  v;
+//         for(auto i:mp)
+//             if(i.second == 1)
+//                 v.push_back(i.first);
+        
+//         int idx=0;
+//         for(int i=0;i<paths.size();i++)
+//             for(int j=0;j<paths[i].size();j++)
+//                 if(v[0]==paths[i][j]){
+//                     idx=j;
+//                     break;
+//                 }
+        
+//         if(idx%2==0)
+//             return v[1];
+//         else
+//             return v[0];
+        int n=paths.size();
         unordered_map <string,int> mp;
         for(int i=0;i<n;i++)
             mp[paths[i][0]]++;
@@ -8,26 +33,7 @@
                 return paths[i][1];
         
         return "";
+           
+    }
 
-        unordered_map <string,int> mp;
-        for(int i=0;i<paths.size();i++)
-            for(int j=0;j<paths[i].size();j++)
-                mp[paths[i][j]]++;
-        
-        vector <string>  v;
-        for(auto i:mp)
-            if(i.second == 1)
-                v.push_back(i.first);
-        
-        int idx=0;
-        for(int i=0;i<paths.size();i++)
-            for(int j=0;j<paths[i].size();j++)
-                if(v[0]==paths[i][j]){
-                    idx=j;
-                    break;
-                }
-        
-        if(idx%2==0)
-            return v[1];
-        else
-            return v[0];
+};
