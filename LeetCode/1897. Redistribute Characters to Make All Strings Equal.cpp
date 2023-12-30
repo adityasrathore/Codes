e5307a876efb7e5a26d29e5199efe34245452785
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2023-12-30 11:26:00 +0530

    Create 1897. Redistribute Characters to Make All Strings Equal.cpp

---
+++ LeetCode/1897. Redistribute Characters to Make All Strings Equal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        int cnt[26] = {0};
+        for (auto &w : words)
+           for (auto ch : w)
+               cnt[ch - 'a']++;
+        
+        
+        int n=words.size();
+        for(auto i: cnt)
+            if(i%n != 0) return false;
+        return true;
+    }
+};
