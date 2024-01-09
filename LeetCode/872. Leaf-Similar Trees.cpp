89250e6849db63895ba6ab632aebd475194ef7d0
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-09 15:00:40 +0530

    Update 872. Leaf-Similar Trees.cpp

--- LeetCode/872. Leaf-Similar Trees.cpp
+++ LeetCode/872. Leaf-Similar Trees.cpp
@@ -26,3 +26,44 @@ public:
         return false;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector <int> r1,r2;
+     void node1(TreeNode* root1){
+        if(root1==NULL)
+            return;
+        node1(root1->left);
+        if(root1->left==NULL && root1->right==NULL)
+            r1.push_back(root1->val);
+        node1(root1->right);
+    }
+    void node2(TreeNode* root2){
+        if(root2==NULL)
+            return;
+        node2(root2->left);
+        if(root2->left==NULL && root2->right==NULL)
+            r2.push_back(root2->val);
+        node2(root2->right);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        node1(root1);
+        node2(root2);
+         
+        if(r1==r2)
+            return true;
+        return false;
+    }
+};
