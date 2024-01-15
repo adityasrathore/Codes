65aa14c600a6eefceb2c64880e296f1f7158a9ea
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-15 14:36:45 +0530

    Update 2225. Find Players With Zero or One Losses.cpp

--- LeetCode/2225. Find Players With Zero or One Losses.cpp
+++ LeetCode/2225. Find Players With Zero or One Losses.cpp
@@ -30,3 +30,38 @@ public:
         return v;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+        vector<vector<int>> v;
+        int n=matches.size();
+        unordered_map <int,int> mp1; 
+        unordered_map <int,int> mp2;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2;j++){
+                if(j==0)
+                    mp1[matches[i][j]]++;
+                else{
+                    mp2[matches[i][j]]++;
+                    mp1[matches[i][j]]=0;
+                }
+            }
+        }
+        vector <int> w;
+        for(auto i:mp1){
+            if(i.second>0 && mp2[i.first]==0)
+                w.push_back(i.first);
+        }
+        vector <int> l;
+        for(auto i: mp2){
+            if(i.second==1)
+                l.push_back(i.first);
+        }
+        sort(w.begin(),w.end());
+        sort(l.begin(),l.end());
+        v.push_back(w);
+        v.push_back(l);
+        return v;
+    }
+};
