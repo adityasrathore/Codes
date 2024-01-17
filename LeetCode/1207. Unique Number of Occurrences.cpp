ae7b6f064841707d5ad6993071472413aea4ad65
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-17 20:31:25 +0530

    Update 1207. Unique Number of Occurrences.cpp

--- LeetCode/1207. Unique Number of Occurrences.cpp
+++ LeetCode/1207. Unique Number of Occurrences.cpp
@@ -15,3 +15,21 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n=arr.size();
+        unordered_map <int,int> mp;
+        for(int i=0;i<n;i++)
+            mp[arr[i]]++;
+        set <int> s;
+        
+        for(auto i:mp){
+            if(s.find(i.second)!=s.end())
+                return false;
+            s.insert(i.second);
+        }
+        return true;
+    }
+};
