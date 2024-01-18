159a9904f0130889bb02e4f9213f1a497da46824
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-18 20:47:35 +0530

    Update 70. Climbing Stairs.cpp

--- LeetCode/70. Climbing Stairs.cpp
+++ LeetCode/70. Climbing Stairs.cpp
@@ -33,3 +33,20 @@ public:
         return curr;
     }
 };
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1)
+            return n;
+        int p1=1;
+        int p2=1;
+        int curr;
+        for(int i=2;i<n+1;i++){
+            curr=p1+p2;
+            p2=p1;
+            p1=curr;
+        }
+        return curr;
+    }
+};
