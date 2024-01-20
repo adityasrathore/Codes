7edf05ae5694d9cdec6f64c8f2447041363d471a
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-20 11:04:19 +0530

    Update 907. Sum of Subarray Minimums.cpp

--- LeetCode/907. Sum of Subarray Minimums.cpp
+++ LeetCode/907. Sum of Subarray Minimums.cpp
@@ -1,1 +1,30 @@
- 
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        stack<int> st;
+        vector<int> prevsmall(n,-1);  
+    
+        st.push(0);
+        for(int i = 1; i<n; i++){
+            while(!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            if(!st.empty()) prevsmall[i]=st.top();
+             st.push(i);
+             
+        }
+		
+        int m = 1e9+7;
+        vector<int> ans(n,0);
+        for(int i = 0; i<n; i++){
+            if(prevsmall[i] == -1) ans[i] = ((i+1)*arr[i])%m;
+            else ans[i]=(ans[prevsmall[i]]+(arr[i]*(i-prevsmall[i]))%m)%m;
+        }
+        
+        int sum=0;
+        for(auto x:ans) 
+            sum=(sum+x)%m;
+        return sum;
+        
+    }
+};
