bb4ab5e997cd5130f2b0c17e13e0341508197471
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-29 12:25:43 +0530

    Update 232. Implement Queue using Stacks.cpp

--- LeetCode/232. Implement Queue using Stacks.cpp
+++ LeetCode/232. Implement Queue using Stacks.cpp
@@ -40,3 +40,50 @@ public:
         return false;
     }
 };
+
+
+--------------
+
+
+    class MyQueue {
+public:
+    stack<int> in;
+    stack<int> out;
+    int ele = -1;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(in.empty()){
+            ele = x;
+        }
+        in.push(x);
+    }
+    
+    int pop() {
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        int value = out.top();
+        out.pop();
+        return value;
+    }
+    
+    int peek() {
+        if(out.empty())
+        return ele;
+
+        return out.top();
+    }
+    
+    bool empty() {
+        if(in.empty() && out.empty())
+        return true;
+
+        return false;
+    }
+};
