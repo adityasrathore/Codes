d95b10d25ef2e1e74919a7e3ee482d9ab8775a00
Aditya Singh Rathore <70751737+adityasrathore@users.noreply.github.com>
2024-01-31 11:15:42 +0530

    Update 739. Daily Temperatures.cpp

--- LeetCode/739. Daily Temperatures.cpp
+++ LeetCode/739. Daily Temperatures.cpp
@@ -25,3 +25,31 @@ public:
         return v;
     }
 };
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        vector <int> v(n);
+        stack <pair<int,int>> s;
+        for(int i=n-1;i>=0;i--){
+            if(!s.empty() && s.top().first>temperatures[i])
+                v[i]=s.top().second;
+            while(!s.empty() && s.top().first<=temperatures[i])
+                s.pop();
+            if(s.empty())
+                v[i]=-1;
+            else
+                v[i]=s.top().second;
+        
+            s.push({temperatures[i],i});
+        }
+        for(int i=0;i<n;i++){
+            if(v[i]!=-1)
+                v[i]=v[i]-i;
+            else
+                v[i]=0;
+        }
+        return v;
+    }
+};
